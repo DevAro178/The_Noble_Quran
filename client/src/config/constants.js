39e816144e4d6c266147fc14d70e06535b5e9a7a
DevAro178 <me.ammad1786@gmail.com>
2026-06-17 18:28:51 +0500

    Pointed backend

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,39 +1,39 @@
-export const API_BASE_URL = '/api';
+export const API_BASE_URL = "https://the-noble-quran-seven.vercel.app/api";
 
 export const TRANSLATIONS = [
   {
-    key: 'quFateh',
-    name: 'Fateh Muhammad Jalandhari',
-    lang: 'Urdu',
-    author: 'Fateh Muhammad',
-    direction: 'rtl'
+    key: "quFateh",
+    name: "Fateh Muhammad Jalandhari",
+    lang: "Urdu",
+    author: "Fateh Muhammad",
+    direction: "rtl",
   },
   {
-    key: 'quMehmood',
-    name: 'Maulana Mahmoodul Hasan',
-    lang: 'Urdu',
-    author: 'Mahmoodul Hasan',
-    direction: 'rtl'
+    key: "quMehmood",
+    name: "Maulana Mahmoodul Hasan",
+    lang: "Urdu",
+    author: "Mahmoodul Hasan",
+    direction: "rtl",
   },
   {
-    key: 'engTaqi',
-    name: 'Mufti Taqi Usmani',
-    lang: 'English',
-    author: 'Mufti Taqi Usmani',
-    direction: 'ltr'
+    key: "engTaqi",
+    name: "Mufti Taqi Usmani",
+    lang: "English",
+    author: "Mufti Taqi Usmani",
+    direction: "ltr",
   },
   {
-    key: 'engMohsin',
-    name: 'Mohsin Khan',
-    lang: 'English',
-    author: 'Muhammad Mohsin Khan',
-    direction: 'ltr'
-  }
+    key: "engMohsin",
+    name: "Mohsin Khan",
+    lang: "English",
+    author: "Muhammad Mohsin Khan",
+    direction: "ltr",
+  },
 ];
 
 export const DEFAULT_TRANSLATIONS = {
   quFateh: true,
   quMehmood: false,
   engTaqi: true,
-  engMohsin: false
+  engMohsin: false,
 };
