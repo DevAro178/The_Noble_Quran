94d4f8c096662d0ddcb69914a14ceadddafd9296
DevAro178 <me.ammad1786@gmail.com>
2026-06-18 17:13:19 +0500

    Repointed Backend

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "https://the-noble-quran-seven.vercel.app/api";
+export const API_BASE_URL = "https://www.pitp.pk/qapi.pitp.pk/api";
 
 export const TRANSLATIONS = [
   {
